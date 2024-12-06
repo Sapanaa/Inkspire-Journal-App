bed59e768ef4637e271b09a8565d8f43384cd744
sapana <sapanadhami1111@gmail.com>
2024-12-06 22:07:12 +0000

    Journals added

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { getCollections } from '../../../actions/collection'
+import { getJournalEntries } from '../../../actions/journal'
+import Collections from './_components/collections'
+const Dashboard = async () => {
+    const collections = await getCollections();
+    const entriesData = await getJournalEntries();
 
-const Dashboard = () => {
+    const entriesByCollection = entriesData?.data.entries.reduce((acc, entry) =>{
+        const collectionId = entry.collectionId || "unorganized";
+        if(!acc[collectionId]){
+        acc[collectionId] = acc[collectionId] || [];
+        }
+        acc[collectionId].push(entry);
+        return acc;
+    }, {})
+
+    console.log(entriesByCollection);
   return (
-    <div>
-      dashboard
+    <div className='px-4 py-8 space-y-8'>
+        <section className='space-y-4'>
+            {/* Mood Analytics */}
+
+        </section>
+        <Collections
+        collections={collections}
+        entriesByCollection={entriesByCollection}
+      />
     </div>
   )
 }
